7da8836402621b874835fcbb2fe2eaac781b7ec2
LEE EUNJI <eunji6986@naver.com>
2022-06-22 04:32:44 +0900

    jwt 인증 + security 인가

---
+++ src/main/java/com/example/jwt/service/CustomUserDetailService.java
@@ -0,0 +1,24 @@
+package com.example.jwt.service;
+
+
+import com.example.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> {
+                    return new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+                });
+    }
+}
