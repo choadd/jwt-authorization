f15103c54a3d3b838b94ed9618bc9c087b34d87d
choadd <choadd@naver.com>
2022-07-03 20:36:38 +0900

    docker-compose..not yet

--- src/main/java/com/example/jwt/controller/DockerTestController.java
+++ src/main/java/com/example/jwt/controller/DockerTestController.java
@@ -25,22 +25,5 @@ public class DockerTestController {
         return new ResponseEntity<List<Test>>(testRepository.findAll(), HttpStatus.OK);
     }
 
-    public static void main(String[] args) {
-        int arr[];
-        int arrlen = arr.length;
-
-        arr = Arrays.stream(arr).distinct().toArray();
-        Arrays.sort(arr);
-        if(arrlen==arr.length){
-            for(int i=0; i<arrlen; i++){
-                if(i==arr[i]){
-
-                }else{
-                }
-            }
-        }
-
-
-    }
 
 }
